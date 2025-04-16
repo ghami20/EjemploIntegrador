d8f5fc1087ccf6b4166a3fff84853801d02438ec
ghami20 <83394687+ghami20@users.noreply.github.com>
2025-04-16 10:12:43 -0300

    Agrego diagrama

--- EjemploException/src/EjemploSingleton/Main.java
+++ EjemploException/src/EjemploSingleton/Main.java
@@ -5,7 +5,6 @@ import javax.swing.JOptionPane;
 public class Main {
 	
 	public static void main(String[] args) {
-		
 		Operario yo = new Operario("Ghami");
 		yo.agregarProducto(new Producto("Maiz",20));
 		int num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese num"));
--- EjemploException/src/Main.java
+++ EjemploException/src/Main.java
@@ -3,24 +3,26 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		try {
-			//try/intentar, intenta realizar una acciòn, esta acciòn puede tener un excepciòn en la ejecucion;
-		int num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese nùmero"));
-		int resultado = 10 / num;
-		int []numeros = new int[1];
-		JOptionPane.showMessageDialog(null, numeros[num]);
-		} catch (NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "Debe ser un nùmero");
-		}catch(ArithmeticException e) {
-			JOptionPane.showMessageDialog(null, "No se puede dividir por 0");
-		}catch(Exception e) {
-			JOptionPane.showMessageDialog(null, "Excepción general");
-		}
-		//NumberFormatException
-		//ArithmeticException
-		JOptionPane.showMessageDialog(null, "El programa sigue");
-		
-	
+
+		int num = ValdiarNum();
+		JOptionPane.showMessageDialog(null, "El programa sigue " + num);
+
+	}
+
+	public static int ValdiarNum() {
+		int num = 0;
+		boolean flag;
+		do {
+			flag = false;
+			try {
+				// try/intentar, intenta realizar una acciòn, esta acciòn puede tener un
+				// excepciòn en la ejecucion;
+				num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese nùmero"));
+				flag = true;
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "Debe ser un nùmero");
+			}
+		} while (flag == false);
+		return num;
 	}
 }
